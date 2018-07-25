ec7eb5251a52254fcacc6f0f09863a4583289b94
Ivan Samoiliuk <bessob@fex.net>
2018-07-25 17:06:09 +0300

    change

--- FormatNumberPi.java
+++ FormatNumberPi.java
@@ -1,17 +1,17 @@
-//пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅ 5 пїЅпїЅпїЅ, 
-//пїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ. 
-//пїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ - 1, пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ - 5
+//Вывести на экран число пи 5 раз, 
+//в каждой строке разное количество знаков после запятой. 
+//В первой строке - 1, в последней - 5
 
-package GitTasks;
+package com.Ivan;
 
 public class FormatNumberPi {
 
 	public static void main(String[] args) {
 		
-		System.out.println(String.format("%.1f%n", Math.PI));	// 1 пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
-		System.out.println(String.format("%.2f%n", Math.PI));	// 2 пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
-		System.out.println(String.format("%.3f%n", Math.PI));	// 3 пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
-		System.out.println(String.format("%.4f%n", Math.PI));	// 4 пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
-		System.out.println(String.format("%.5f%n", Math.PI));	// 5 пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+		System.out.println(String.format("%.1f%n", Math.PI));	// 1 цифра после запятой
+		System.out.println(String.format("%.2f%n", Math.PI));	// 2 цифры после запятой
+		System.out.println(String.format("%.3f%n", Math.PI));	// 3 цифры после запятой
+		System.out.println(String.format("%.4f%n", Math.PI));	// 4 цифры после запятой
+		System.out.println(String.format("%.5f%n", Math.PI));	// 5 цифры после запятой
 	}
 }
